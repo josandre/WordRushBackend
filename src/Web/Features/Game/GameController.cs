a204797e3bb6c288da8b8af19bfa374e395af6b5
Francisco-Tovar <55120226+Francisco-Tovar@users.noreply.github.com>
2025-09-09 18:24:15 -0600

    Added CORS

--- src/Web/Features/Game/GameController.cs
+++ src/Web/Features/Game/GameController.cs
@@ -1,7 +1,8 @@
+using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
 
 namespace WordRush.Web.Features.Game;
-
+[EnableCors]
 [ApiController]
 [Route("api/games")]
 public class GameController : ControllerBase
@@ -9,6 +10,6 @@ public class GameController : ControllerBase
     [HttpGet]
     public IActionResult Get()
     {
-        return Ok();
+        return Ok(new { message = "Hello Brain-Hub" });
     }
 }

ff9068905bfdf534c026067a5491b8adb15c0ec2
Francisco Tovar <55120226+Francisco-Tovar@users.noreply.github.com>
2025-09-30 21:06:20 -0600

    added logs to services (#9)

--- src/Web/Controllers/AuthController.cs
+++ src/Web/Controllers/AuthController.cs
@@ -2,114 +2,109 @@ using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Serilog;
 using WordRush.Core.Features;
 using WordRush.Core.Infrastructure.Identity;
 using WordRush.Core.Infrastructure.Identity.Models;
 using WordRush.Repository.Models;
 using WordRush.Web.Models;
 
-namespace WordRush.Web.controllers;
-
-[Route("auth")]
-public class AuthController : ApiControllerBase
+namespace WordRush.Web.Controllers
 {
-  private readonly SignInManager<User> signInManager;
-  private readonly UserManager<User> userManager;
-  private readonly IAuthService authService;
-  private readonly IRoleService roleService;
-
-
-  public AuthController(SignInManager<User> signInManager, IAuthService authService, UserManager<User> userManager, IRoleService roleService)
+  [Route("auth")]
+  public class AuthController(SignInManager<User> signInManager, IAuthService authService, UserManager<User> userManager, IRoleService roleService) : ApiControllerBase
   {
-    this.signInManager = signInManager;
-    this.userManager = userManager;
-    this.roleService = roleService;
-    this.authService = authService;
-  }
+    private readonly SignInManager<User> signInManager = signInManager;
+    private readonly UserManager<User> userManager = userManager;
+    private readonly IAuthService authService = authService;
+    private readonly IRoleService roleService = roleService;
 
-  [HttpPost("login")]
-  [AllowAnonymous]
-  public async Task<ActionResult<LoginResponse>> SignIn([FromBody] LoginRequest request)
-  {
-    var user = await this.GetUser(request.Email);
-
-    if (user != null)
+    [HttpPost("login")]
+    [AllowAnonymous]
+    public async Task<ActionResult<LoginResponse>> SignIn([FromBody] LoginRequest request)
     {
-      var passValidationResult = await this.signInManager.CheckPasswordSignInAsync(user, request.Password, true);
+      User? user = await GetUser(request.Email);
 
-      if (passValidationResult.Succeeded)
+      if (user != null)
       {
-        var response = await this.authService.Login(user);
-        return this.Ok(response);
+        Microsoft.AspNetCore.Identity.SignInResult passValidationResult = await signInManager.CheckPasswordSignInAsync(user, request.Password, true);
+
+        if (passValidationResult.Succeeded)
+        {
+          LoginResponse response = await authService.Login(user);
+
+          Log.Information(messageTemplate: "Authorization service Result => {@Response}", response);
+
+          return Ok(response);
+        }
+
+        return BadRequest("Incorrect password");
       }
 
-      return this.BadRequest("Incorrect password");
+      return BadRequest("Incorrect email");
     }
 
-    return this.BadRequest("Incorrect email");
-  }
-
-  [HttpPost("sign-up")]
-  [AllowAnonymous]
-  public async Task<ActionResult<SignUpResponse>> SignUp([FromBody] SignUpRequest request)
-  {
-    var user = await CreateUserFromRequest(request);
-    var response = await this.userManager.CreateAsync(user, request.Password);
-
-    if (!response.Succeeded)
+    [HttpPost("sign-up")]
+    [AllowAnonymous]
+    public async Task<ActionResult<SignUpResponse>> SignUp([FromBody] SignUpRequest request)
     {
-      var signUpResponse = new SignUpResponse()
+      User user = await CreateUserFromRequest(request);
+      IdentityResult response = await userManager.CreateAsync(user, request.Password);
+
+      if (!response.Succeeded)
       {
-        Success = false,
-        Message = string.Join("; ", response.Errors.Select(e => e.Description))
+        SignUpResponse signUpResponse = new()
+        {
+          Success = false,
+          Message = string.Join("; ", response.Errors.Select(e => e.Description))
+        };
+
+        return BadRequest(signUpResponse);
+      }
+
+      SignUpResponse signup = new()
+      {
+        Success = true,
+        Message = "User registered successfully",
+        UserId = user.Id,
+        UserName = user.UserName,
+        Email = user.Email,
+        Nickname = user.Nickname,
+        Avatar = user.Avatar,
+        RoleId = user.RoleId
       };
+      Log.Information(messageTemplate: "Signup Service Result => {@Signup}", signup);
 
-      return this.BadRequest(signUpResponse);
+      return signup;
     }
 
-    return new SignUpResponse
+    private async Task<User> CreateUserFromRequest(SignUpRequest request)
     {
-      Success = true,
-      Message = "User registered successfully",
-      UserId = user.Id,
-      UserName = user.UserName,
-      Email = user.Email,
-      Nickname = user.Nickname,
-      Avatar = user.Avatar,
-      RoleId = user.RoleId
-    };
-  }
-
-  private async Task<User> CreateUserFromRequest(SignUpRequest request)
-  {
-
-    Role role = await roleService.GetRoleById(1);
-
-    var user = new User
-    {
-      UserName = request.UserName,
-      Email = request.Email,
-      Avatar = request.Avatar,
-      Nickname = request.Nickname,
-      Role = role
-    };
-
-
-    return user;
-  }
-
-
-
-  private async Task<User> GetUser(string identifier)
-  {
-    var isEmail = new EmailAddressAttribute().IsValid(identifier);
-
-    if (!isEmail)
-    {
-      return null;
+      Role role = await roleService.GetRoleById(1);
+      User user = new()
+      {
+        UserName = request.UserName,
+        Email = request.Email,
+        Avatar = request.Avatar,
+        Nickname = request.Nickname,
+        Role = role
+      };
+      Log.Information(messageTemplate: "Create User Service Result => {@User}", user);
+      return user;
     }
 
-    return await this.signInManager.UserManager.FindByEmailAsync(identifier);
-  }
+    private async Task<User?> GetUser(string identifier)
+    {
+      bool isEmail = new EmailAddressAttribute().IsValid(identifier);
 
+      if (!isEmail)
+      {
+        return null;
+      }
+
+      User? user = await signInManager.UserManager.FindByEmailAsync(identifier);
+      Log.Information(messageTemplate: "Get User Service Result => {@User}", user);
+      return user;
+    }
+  }
 }
